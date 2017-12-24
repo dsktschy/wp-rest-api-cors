d558652748c0a702cb737546c4f39472fa881bda
dai <chamestica@gmail.com>
2017-12-25 02:25:37 +0900

    Use a class as a namespace

--- functions.php
+++ functions.php
@@ -1,41 +1,7 @@
 <?php
-$fields = [
-  'origin' => [
-    'id' => 'access_control_allow_origin',
-    'title' => [
-      'en' => 'Origins allowed requests to REST API',
-      'ja' => 'REST APIへのリクエストを許可するオリジン'
-    ],
-    'callback' => 'echo_input',
-    'page' => 'general',
-    'section' => 'default'
-  ],
-  'webhook' => [
-    'id' => 'webhook_url',
-    'title' => [
-      'en' => 'Webhook URL when saving posts',
-      'ja' => '保存時にリクエストを送るWebhookのURL'
-    ],
-    'callback' => 'echo_input',
-    'page' => 'general',
-    'section' => 'default'
-  ],
-  'redirect' => [
-    'id' => 'redirect_url',
-    'title' => [
-      'en' => 'Redirection URL for requests to pages',
-      'ja' => 'ページへのリクエストに対するリダイレクトURL'
-    ],
-    'callback' => 'echo_input',
-    'page' => 'general',
-    'section' => 'default'
-  ]
-];
-
 // Add fields to the setting page
 add_filter('admin_init', function() {
-  global $fields;
-  foreach ($fields as $field) {
+  foreach (Restapi::$settingsFields as $field) {
     add_settings_field(
       $field['id'],
       $field['title'][preg_match('/^ja/', get_option('WPLANG')) ? 'ja' : 'en'],
@@ -53,11 +19,12 @@ add_filter('admin_init', function() {
 add_action('rest_api_init', function() {
   remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
   add_filter('rest_pre_serve_request', function($value) {
-    global $fields;
     $origin = get_http_origin();
     if ($origin && in_array(
       $origin,
-      explode(',', str_replace(' ', '', get_option($fields['origin']['id'])))
+      explode(',', str_replace(' ', '', get_option(
+        Restapi::$settingsFields['origin']['id']
+      )))
     )) {
       header('Access-Control-Allow-Origin: ' . esc_url($origin));
       header('Access-Control-Allow-Methods: GET, OPTIONS');
@@ -69,8 +36,9 @@ add_action('rest_api_init', function() {
 
 // Send a post request to webhooks when saving posts
 add_action('save_post', function() {
-  global $fields;
-  $urls = explode(',', str_replace(' ', '', get_option($fields['webhook']['id'])));
+  $urls = explode(',', str_replace(' ', '', get_option(
+    Restapi::$settingsFields['webhook']['id']
+  )));
   foreach ($urls as $url) {
     $ch = curl_init(esc_url($url));
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -80,8 +48,46 @@ add_action('save_post', function() {
   }
 });
 
-function echo_input(array $args) {
-  $id = $args['id'];
-  $value = esc_html(get_option($id));
-  echo "<input name=\"$id\" id=\"$id\" type=\"text\" value=\"$value\" class=\"regular-text code\">";
+// Class as a namespace
+class Restapi
+{
+  static public $settingsFields = [
+    'origin' => [
+      'id' => 'access_control_allow_origin',
+      'title' => [
+        'en' => 'Origins allowed requests to REST API',
+        'ja' => 'REST APIへのリクエストを許可するオリジン'
+      ],
+      'callback' => ['Restapi', 'echoInput'],
+      'page' => 'general',
+      'section' => 'default'
+    ],
+    'webhook' => [
+      'id' => 'webhook_url',
+      'title' => [
+        'en' => 'Webhook URL when saving posts',
+        'ja' => '保存時にリクエストを送るWebhookのURL'
+      ],
+      'callback' => ['Restapi', 'echoInput'],
+      'page' => 'general',
+      'section' => 'default'
+    ],
+    'redirection' => [
+      'id' => 'redirect_url',
+      'title' => [
+        'en' => 'Redirection URL for requests to pages',
+        'ja' => 'ページへのリクエストに対するリダイレクトURL'
+      ],
+      'callback' => ['Restapi', 'echoInput'],
+      'page' => 'general',
+      'section' => 'default'
+    ]
+  ];
+  // Outputs an input element with initial value
+  static public function echoInput(array $args)
+  {
+    $id = $args['id'];
+    $value = esc_html(get_option($id));
+    echo "<input name=\"$id\" id=\"$id\" type=\"text\" value=\"$value\" class=\"regular-text code\">";
+  }
 }
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
-// Redirect if specified
-$url = explode(',', str_replace(' ', '', get_option('redirect_url')))[0];
-if ($url !== '') {
-  header('Location: ' . esc_url($url));
-  exit;
-}
+(function() {
+  // Redirect if specified
+  $url = explode(',', str_replace(' ', '', get_option('redirect_url')))[0];
+  if ($url !== '') {
+    header('Location: ' . esc_url($url));
+    exit;
+  }
+})();
 ?>
 <!DOCTYPE html>
 <html lang="en">
