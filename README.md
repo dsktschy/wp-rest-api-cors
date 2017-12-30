ed896c214ce90f2d532fc3a164b392f3ba45514a
dai <chamestica@gmail.com>
2017-12-30 17:27:14 +0900

    Rewrite as a plug-in and separate for each function. WP REST API CORS, WP Redirector, and WP Ping Expander

---
+++ README.md
@@ -0,0 +1,5 @@
+# WP REST API CORS
+WP REST API CORS is a WordPress plugin.  
+It allows use of REST API only for the specified origins.  
+Set origins URLs on "Settings" > "General" > "Origins allowed requests to REST API".  
+Note: There should not be a slash at the end of the request origin.
---
+++ wp-rest-api-cors/index.php
@@ -0,0 +1,47 @@
+<?php
+// Add fields to the setting page
+add_filter('admin_init', function() {
+  add_settings_field(
+    WpRestApiCors::$fieldId,
+    preg_match('/^ja/', get_option('WPLANG')) ?
+      'REST APIへのリクエストを許可するオリジン' :
+      'Origins allowed requests to REST API',
+    ['WpRestApiCors', 'echoField'],
+    WpRestApiCors::$fieldPage,
+    'default',
+    ['id' => WpRestApiCors::$fieldId]
+  );
+  register_setting(WpRestApiCors::$fieldPage, WpRestApiCors::$fieldId);
+});
+
+// Allow use of REST API only for the specified origins
+// Note: There's no slash at the end of the request origin
+add_action('rest_api_init', function() {
+  if (get_option(WpRestApiCors::$fieldId) === '') return;
+  remove_filter('rest_pre_serve_request', 'rest_send_cors_headers');
+  add_filter('rest_pre_serve_request', function($value) {
+    $origin = get_http_origin();
+    if ($origin && in_array($origin, explode(',', str_replace(
+      ' ', '', get_option(WpRestApiCors::$fieldId)
+    )))) {
+      header('Access-Control-Allow-Origin: ' . esc_url($origin));
+      header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+      header('Access-Control-Allow-Credentials: true');
+    }
+    return $value;
+  });
+}, 15);
+
+// Class as a namespace
+class WpRestApiCors
+{
+  static public $fieldId = 'access_control_allow_origin';
+  static public $fieldPage = 'general';
+  // Outputs an input element with initial value
+  static public function echoField(array $args)
+  {
+    $id = $args['id'];
+    $value = esc_html(get_option($id));
+    echo "<input name=\"$id\" id=\"$id\" type=\"text\" value=\"$value\" class=\"regular-text code\">";
+  }
+}
